55e6cf3c6dd080e13fa0d923f92180ae15a46799
molodezh0 <platinov133@gmail.com>
2026-03-21 15:29:43 +1000

    Create Triangle.h

---
+++ Lab4/Triangle.h
@@ -0,0 +1,6 @@
+#ifndef TRIANGLE_H
+#define TRIANGLE_H
+double calc_perimeter(double a, double b, double c);
+double calc_area(double a, double b, double с);
+int check_triangle(double a, double b, double c);
+#endif
